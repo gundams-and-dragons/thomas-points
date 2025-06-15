c68468441e30412d6b4dbab4ff31c6120d2d2737
gundams-and-dragons <warren@tabletoptoolbox.com>
2025-06-14 19:12:58 -0700

    adding database integration

--- app/api/users/[name]/route.ts
+++ app/api/users/[name]/route.ts
@@ -1,16 +1,30 @@
-import {  NextResponse, NextRequest } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
+import clientPromise from '@/lib/mongodb';
 
 export async function GET(
   _request: NextRequest,
   { params }: { params: Promise<{ name: string }> },
 ) {
   const { name } = await params;
+  const client = await clientPromise;
+  const db = client.db('thomas_points');
+  const user = await db.collection('users').findOne({ name });
 
+  if (!user) {
+    return NextResponse.json(
+      { error: 'User not found' },
+      {
+        status: 404,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+  
   return NextResponse.json(
-    { message: `GET user ${name}` },
+    { user },
     {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     }
   );
-}
+};
